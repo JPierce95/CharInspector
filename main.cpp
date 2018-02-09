92f723e2a99d96500f457732673d25fb97e3b0b2
JPierce95 <35712472+JPierce95@users.noreply.github.com>
2018-02-08 23:26:14 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //Author:
 #include<iostream>
 
-using namespace std::;
+using namespace std;
 
 int main()
 {
@@ -10,7 +10,7 @@ int main()
   char lower [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
   char upper [] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
   cout<<"What character do you want to know about?\n";
-  cin Char;
+  cin >> Char;
   int ascii = Char;
 
   for(int i = 0; i < 26; i++){
